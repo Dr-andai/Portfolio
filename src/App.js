e9f7658bf29c7c7df8157c15a8a700e8c64cf436
TheSPIDERshadow <68011723+TheSPIDERshadow@users.noreply.github.com>
2022-07-15 23:14:18 +0300

    image added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import React from 'react';
+import HomePage from './HomePage';
 
 function App() {
   return (
-    <div className="App">
+    <div >
+      <HomePage />
       
     </div>
   );
